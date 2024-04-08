9199fd8eac6abd32b1fcd9d059d27a44e6f52c75
cf1048596 <christianflorencio2004@gmail.com>
2024-04-08 20:38:35 +1200

    fixed build system for c# nlp

--- nlp/Program.cs
+++ nlp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Catalyst;
+using Mosaik.Core;
+
+namespace NLP;
+
+public class Program {
+  public static async Task Main() {
+    Catalyst.Models.English.Register(); //You need to pre-register each language (and install the respective NuGet Packages)
+    Storage.Current = new DiskStorage("catalyst-models");
+    var nlp = await Pipeline.ForAsync(Language.English);
+    var doc = new Document("The quick brown fox jumps over the lazy dog", Language.English);
+    nlp.ProcessSingle(doc);
+    Console.WriteLine(doc.ToJson());
+  }
+}
+
