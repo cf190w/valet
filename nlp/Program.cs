07e2c4700e22a4b969e1490df8611947991e304a
Kai Meiklejohn <kaim.meiklejohn@gmail.com>
2024-04-09 21:22:19 +1200

    changed the input text and shows both original and new json outputs

--- nlp/Program.cs
+++ nlp/Program.cs
@@ -9,8 +9,9 @@ public class Program {
     Catalyst.Models.English.Register(); //You need to pre-register each language (and install the respective NuGet Packages)
     Storage.Current = new DiskStorage("catalyst-models");
     var nlp = await Pipeline.ForAsync(Language.English);
-    var doc = new Document("The quick brown fox jumps over the lazy dog", Language.English);
+    var doc = new Document("Hey Valet. Copy and paste this text", Language.English);
     nlp.ProcessSingle(doc);
+    Console.WriteLine(doc.ToJson());
     string prettyJson = JsonConvert.SerializeObject(doc, Formatting.Indented);
     Console.WriteLine(prettyJson);
   }
