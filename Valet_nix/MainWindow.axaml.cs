39a64d2b5dc49f8a9dd0fa95b60348c4153de36d
cf1048596 <christianflorencio2004@gmail.com>
2024-05-12 23:54:23 +1200

    reformatted window size

--- Valet_nix/MainWindow.axaml.cs
+++ Valet_nix/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia;
+using Avalonia.Markup.Xaml;
 
 namespace Valet_nix;
 
@@ -7,5 +9,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void InitializeComponent() {
+        AvaloniaXamlLoader.Load(this);
+        //Set width & height of the window 
+        Width = 462;
+        Height = 800;
     }
 }
