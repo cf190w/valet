b23f5225e24f5ce312b2b9b5c2916a086a34bc72
cf1048596 <christianflorencio2004@gmail.com>
2024-05-25 15:17:59 +1200

    can change the value of a textbox

--- Valet_nix/Views/MainWindow.axaml.cs
+++ Valet_nix/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using System;
+using Avalonia.Interactivity;
 
 namespace Valet_nix.Views;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ButtonOnClick(object? sender, RoutedEventArgs e) {
+      Console.WriteLine("hello button hit");
+      TextBlockName.Text = "CLICKED";
     }
 }
